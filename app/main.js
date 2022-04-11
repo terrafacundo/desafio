ba22223f2bcbdfb567a8160e777857b346b19742
disenofgti <terra.facundo.devacc@gmail.com>
2022-04-11 20:12:11 -0300

    funcionalidad basica implementada

--- app/main.js
+++ app/main.js
@@ -9,34 +9,19 @@
 
 
 function app(){
-
 //constructor de actividades
-class Entrada {
-    constructor(nombre,horas,sensacion) {
-        this.nombre = nombre;
-        this.nombre=horas;
-        this.sensacion=sensacion
+let actividades = [];
+class Actividad {
+    constructor(nombre,hora,sensacion){
+        this.nombre=nombre;
+        this.hora=hora;
+        this.sensacion=sensacion;
     }
-
 }
 
 
-//el cuadrante uno = contiene los ingresos de urg imp, el dos = no urg imp, el tres=urg no imp, el cuatro= no urg no imp
-let cuadranteUno = [];
-let cuadranteDos = [];
-let cuadranteTres = [];
-let cuadranteCuatro = [];
-
 //declarando las arrays,variables más importantes, tomando los nodos más importantes:
-let lunes =[];
-let martes=[];
-let miercoles=[];
-let jueves=[];
-let viernes=[];
-let sabado =[];
-let domingo=[];
 
-let semana=[lunes,martes,miercoles,jueves,viernes,sabado,domingo];
 
 //nodos e interacción de la info extra
 let info_extra = document.getElementById("info");
@@ -45,20 +30,6 @@ let info_extra = document.getElementById("info");
 
 let boxUno = document.getElementById("urgimp");
 boxUno.addEventListener("click",mas_info);
-
-let boxDos = document.getElementById("nurgimp");
-boxUno.addEventListener("click",mas_info);
-
-let boxtres = document.getElementById("urgnimp");
-boxUno.addEventListener("click",mas_info);
-
-let boxCuatro= document.getElementById("nurgnimp");
-boxUno.addEventListener("click",mas_info);
-
-
-
-
-
 
 
 //funciones
@@ -73,40 +44,43 @@ function mas_info(){
 
     //FORMULARIO
 
-    let formulario = document.createElement("form");
-    formulario.setAttribute("class","formu");
-    formulario.innerHTML=`<label>Actividad: <input id="actividad" type="text" required></input></label>
-    <label>Horas dedicadas:<input id="horas" type="number" required></input></label>
-    <label>Sensación al hacerla: <input id="sensacion" type="text" required></input></label>
-    <label><input type="submit" value="Enviar"></input></label>
-    <div id="error"></div>`;
+        let formulario = document.createElement("form");
+        formulario.setAttribute("class","formu");
+        formulario.innerHTML=`<label>Actividad: <input id="actividad" type="text" required></input></label>
+        <label>Horas dedicadas:<input id="horas" type="number" required></input></label>
+        <label>Sensación al hacerla: <input id="sensacion" type="text" required></input></label>
+        <label><input type="submit" value="Enviar"></input></label>
+        <div id="error"></div>`;
+        info_extra.append(formulario);
 
-    //tomando los nodos de los input, e insertandolo en el body
-    let actividad=document.getElementById("actividad");
-    let horas=document.getElementById("horas");
-    let sensacion = document.getElementById("sensacion");
-    info_extra.append(formulario);
-    
+        //tomando los nodos de los input, e insertandolo en el body
+        let actividad = document.getElementById("actividad");
+        let horas= document.getElementById("horas");
+        let sensacion = document.getElementById("sensacion");
+        
 
-    
+        //validando datos del formulario
+        formulario.addEventListener("submit",validando_formulario);
+        function validando_formulario(e){
+            e.preventDefault();
+            let notificacion =document.createElement("h2");
+            notificacion.setAttribute("class","noti");
+            notificacion.innerHTML=`Actividad agregada con éxito`;
+            formulario.append(notificacion);
 
-
-    //validando datos del formulario
-    formulario.addEventListener("submit",validando_formulario);
-    function validando_formulario(e){
-        e.preventDefault();
-        let notificacion =document.createElement("h2");
-        notificacion.setAttribute("class","noti");
-        notificacion.innerHTML=`Actividad agregada con éxito`;
-        formulario.append(notificacion);
-        ;
-    }
-
+            valor_actividad = actividad.value;
+            valor_horas = horas.value;
+            valor_sensacion =sensacion.value;
+            
+            let entrada =  new Actividad(valor_actividad, valor_horas,valor_sensacion);
+            actividades.push(entrada);
+        }
     //parte interactiva del formulario
-    //validacion de datos para poder pushear
-    console.log(actividad.value)
+    //validacion de datos
+    
+
         
 }
-console.log(semana);
+console.log(actividades);
 }
 
